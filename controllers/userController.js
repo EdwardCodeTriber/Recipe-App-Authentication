4ee5bfeec02d8fd33cf00ff5679d04a7a45ccf92
Thapelo Somo <edwardthapelo55@gmail.com>
2024-10-28 16:29:45 +0200

    Added a register function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import bcrypt from "bcrypt";
+import generateToken from "../utils/index.js";
 
 const registerUser = async (req, res) => {
     try {
@@ -29,4 +30,34 @@ const registerUser = async (req, res) => {
         }
     }
 };
+
+
+const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const user = User.findOne({email})
+
+        if(!user || !(await user.matchPassword(password))){
+            return res.status(401).json({
+                error: "Invalid login credentials"
+            })
+        }
+
+        const token = await generateToken(user._id);
+        res.status(200).json({
+            _id: user._id, 
+            email: user.email,
+            token
+        })
+    } catch (error) {
+        res.status(500).json({
+            error: "Internal server error"
+        })
+    }
+}
+
+export default {
+    registerUser,
+    loginUser,
+}
 
