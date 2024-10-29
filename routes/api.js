c8fe71c4f7436cd93d88d6759139c72a63a9d34e
Thapelo Somo <edwardthapelo55@gmail.com>
2024-10-29 16:19:44 +0200

    Used the protection route for authentication check

--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,16 @@
 import express from "express"
 import receipeController from "../controllers/receipeController.js";
 import userController from "../controllers/userController.js";
+import protect from "../middleware/authMiddleware.js"
 //Router paths to execute endpoins
 const router = express.Router()
 //Routes
 router.post("/user", userController.registerUser)
 router.post("/user/login", userController.loginUser)
-router.post("/reciepe", receipeController.createReceipe)
-router.get("/reciepe", receipeController.getReceipes)
-router.get("/reciepe/:id", receipeController.getReceipe)
-router.delete("/reciepe/:id", receipeController.deleteReceipe)
-router.put("/reciepe/:id", receipeController.updateReceipe)
+router.post("/reciepe", protect, receipeController.createReceipe)
+router.get("/reciepe", protect, receipeController.getReceipes)
+router.get("/reciepe/:id", protect, receipeController.getReceipe)
+router.delete("/reciepe/:id", protect, receipeController.deleteReceipe)
+router.put("/reciepe/:id", protect, receipeController.updateReceipe)
 
 export default router;
