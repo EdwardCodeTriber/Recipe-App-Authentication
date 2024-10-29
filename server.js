0ff93b65a9692ff7bb8c68c940f753c159483fd1
Thapelo Somo <edwardthapelo55@gmail.com>
2024-10-29 15:17:13 +0200

    Added a body parser to save details(register)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import express from "express"
 import cors from "cors"
 import connectDB from "./config/database.js";
 import router from "./routes/api.js";
+import bodyParser from "body-parser"
 
 //App start
 const app = express();
@@ -12,7 +13,8 @@ app.use(express.json())
 app.use(cors());
 // App use imported router with endpoints into an active server/api
 app.use("/api", router)
-
+app.use(bodyParser.json()); // Parses incoming JSON requests
+app.use(bodyParser.urlencoded({ extended: true })); // Parses URL-encoded data
 
 // MongoDB connection with the .env file
 connectDB()
